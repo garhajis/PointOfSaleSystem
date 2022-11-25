07547377829e3a57a1fffff49f9e43d8e24c72b3
garhajis <118834668+garhajis@users.noreply.github.com>
2022-11-25 18:58:49 +0300

    Add files via upload

---
+++ point of sale.c
@@ -0,0 +1,10 @@
+//Point-Of-Sale
+#include<stdio.h>
+void main(void){
+    int price;
+    printf("Rice,price=350,VAT at 16%");
+    printf("Sugar,price=320,VAT at 16%");
+    scanf("%d",price);
+
+return 0;
+}
